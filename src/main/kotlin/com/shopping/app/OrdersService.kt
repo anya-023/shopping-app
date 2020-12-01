0bd8ac940188e8b9153d09e7da17cb3bdc4ee4bf
anya-023 <66692264+anya-023@users.noreply.github.com>
2020-12-01 23:01:45 +0530

    Offer Applied/tests modified
    Commiting Step 2: Simple offer

--- src/main/kotlin/com/shopping/app/OrdersService.kt
+++ src/main/kotlin/com/shopping/app/OrdersService.kt
@@ -21,9 +21,10 @@ fun calculatePrice(itemsStr: String): Double {
         } else {
             //get count of each item entered
             val itemCount = items.groupingBy { it }.eachCount()
-
-            //get the total amount for entered items
-            itemCount.forEach { (k, v) ->
+            //apply offers - b1g1 for apple , b3 pay for 2 for orange
+            val offerMap = applyOffer(itemCount)
+            //get the total amount for entered items post offer
+            offerMap.forEach { (k, v) ->
                 total += availableItems.first { item -> item.name == k }.price * (v)
             }
 
@@ -34,6 +35,26 @@ fun calculatePrice(itemsStr: String): Double {
     }
 
     return total
+}
+
+//apply offer for apple and orange
+fun applyOffer(itemCount: Map<String, Int>): Map<String, Int> {
+
+    return mapOf("apple" to calculateOffer(itemCount["apple"], 2),
+        "orange" to calculateOffer(itemCount["orange"], 3))
+
+}
+//logic for calculating total items charged post offer
+fun calculateOffer(count: Int?, offer: Int): Int {
+    var offerCount: Int = 0
+    if (count != null) {
+        offerCount = if (count < offer) {
+            count
+        } else {
+            count - (count / offer)
+        }
+    }
+    return offerCount;
 }
 
 
--- src/test/kotlin/com/shopping/app/ShoppingAppTest.kt
+++ src/test/kotlin/com/shopping/app/ShoppingAppTest.kt
@@ -5,12 +5,29 @@ import kotlin.test.Test
 
 class ShoppingAppTest {
 
-    @Test
+   /* @Test
     fun calculatePriceTest() {
         Assertions.assertEquals(2.05, calculatePrice("apple,apple,orange,apple"))
         Assertions.assertEquals(0.00, calculatePrice("apple,apple,stone,apple"))
         Assertions.assertEquals(0.00, calculatePrice(""))
         Assertions.assertEquals(2.05, calculatePrice("apple,apple,ORANGE,apple"))
+    }*/
+
+
+    @Test
+    fun offerPriceTest() {
+        Assertions.assertEquals(1.45, calculatePrice("apple,apple,orange,apple"))
+        Assertions.assertEquals(0.00, calculatePrice("apple,apple,stone,apple"))
+        Assertions.assertEquals(0.00, calculatePrice(""))
+        Assertions.assertEquals(1.6, calculatePrice("apple,apple,ORANGE,orange,orange,orange,orange,orange"))
+        Assertions.assertEquals(.85, calculatePrice("apple,orange"))
+        Assertions.assertEquals(1.1, calculatePrice("apple,ORANGE,orange"))
+        Assertions.assertEquals(6.75, calculatePrice("orange,orange,orange,orange,orange," +
+                "orange,orange,orange,orange,orange,orange,orange," +
+                "orange,orange,orange,orange,orange,orange,orange,orange,orange," +
+                "orange,orange,orange,orange,orange,orange,orange,orange,orange,orange,orange," +
+                "orange,orange,orange,orange,orange,orange,orange,orange"))
+        Assertions.assertEquals(4.20, calculatePrice("apple,apple,apple,apple,apple,apple,apple,apple,apple,apple,apple,apple,apple"))
     }
 
 }
